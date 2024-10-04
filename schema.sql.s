88de20f3e772f68036f7428a4a28a593a69c47e0
“firstname <daniel987@gmail.com>
2024-10-04 14:17:23 -0400

    adding sql files

---
+++ schema.sql.s
@@ -0,0 +1,10 @@
+CREATE TABLE topics(
+    id serial PRIMARY KEY,
+    topic_name text
+)
+
+CREATE TABLE rankings(
+    id serial PRIMARY KEY,
+    ranking_name text
+    
+)
---
+++ seed.sql.s
@@ -0,0 +1,12 @@
+INSERT INTO rankings (ranking_name, value) VALUES
+    ('Awesome', 5) 
+    ('Great', 4),
+    ('Good', 3),
+    ('Okay', 2),
+    ('Poor', 1),
+    ('Unranked', 0);
+
+INSERT INTO topics (topic_name) VALUES
+    ('JavaScript'),
+    ('HTML'),
+    ('CSS');
